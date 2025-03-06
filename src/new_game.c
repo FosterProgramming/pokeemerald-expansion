c08dd9a20ca724e32222584aed17da795a5e8141
Heracross1991 <heracross1991@gmail.com>
2025-03-06 10:37:28 -0500

    changed player sprite from brendan to dewgong and added a new anim table for his running frames

--- src/new_game.c
+++ src/new_game.c
@@ -127,7 +127,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(DOMINION_CITY_MAGA_LAB_HOLDING_CELLS), MAP_NUM(DOMINION_CITY_MAGA_LAB_HOLDING_CELLS), WARP_ID_NONE, 6, 3);
     WarpIntoMap();
 }
 
