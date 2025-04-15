d795c31adaf4207b4f82873ee372f23d7daff1b0
Heracross1991 <heracross1991@gmail.com>
2025-04-15 10:36:42 -0400

    moonthistle trust quest complete

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -18,7 +18,7 @@
 #define TRAINER_INTRO_CUTSCENE_FRO                      12
 #define TRAINER_ED                           13
 #define TRAINER_MAGA_GRUNT_VOLTBROOK_TOWN      14
-#define TRAINER_DECLAN                       15
+#define TRAINER_ROUTE_13_MAGA_GRUNT                       15
 #define TRAINER_GRUNT_RUSTURF_TUNNEL         16
 #define TRAINER_GRUNT_WEATHER_INST_1         17
 #define TRAINER_GRUNT_WEATHER_INST_2         18
