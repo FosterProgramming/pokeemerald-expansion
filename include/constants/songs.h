c07d31abafdf27219170266cade4e22d43368a08
Heracross1991 <heracross1991@gmail.com>
2025-05-26 13:46:54 -0400

    added mus_sv_vee

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -549,6 +549,7 @@
 // Seel Version Music
 
 #define MUS_SV_MAIN_MENU            610 // Expedition 33: Clair
+#define MUS_SV_MAIN_VEE            611 // Expedition 33: Gustave
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
