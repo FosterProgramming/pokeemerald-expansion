8f89ac08b54fed94b66e752a89e2f3f5a885465b
Heracross1991 <heracross1991@gmail.com>
2025-05-28 12:25:27 -0400

    song stuff

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -549,7 +549,8 @@
 // Seel Version Music
 
 #define MUS_SV_MAIN_MENU            610 // Expedition 33: Clair
-#define MUS_SV_MAIN_VEE            611 // Expedition 33: Gustave
+#define MUS_SV_MAIN_VEE             611 // Expedition 33: Gustave
+#define MUS_SV_GLACIERRA            612 // Octopath Traveller: Stillsnow
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
