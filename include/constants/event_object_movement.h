harmakanna/TARC2

Add new movement actions: shake_horizontal and shake_vertical

include/constants/event_object_movement.h
@@ -259,6 +259,8 @@
 #define MOVEMENT_ACTION_SURF_STILL_UP                   0xAB
 #define MOVEMENT_ACTION_SURF_STILL_LEFT                 0xAC
 #define MOVEMENT_ACTION_SURF_STILL_RIGHT                0xAD
+#define MOVEMENT_ACTION_SHAKE_HORIZONTAL                0xAE
+#define MOVEMENT_ACTION_SHAKE_VERTICAL                  0xAF
 
 #define MOVEMENT_ACTION_STEP_END 0xFE
 #define MOVEMENT_ACTION_NONE     0xFF

FosterProgramming/pokeemerald-expansion

include/constants/event_object_movement.h
@@ -266,6 +266,9 @@
 #define MOVEMENT_ACTION_WALK_FAST_DIAGONAL_DOWN_LEFT    0xB0
 #define MOVEMENT_ACTION_WALK_FAST_DIAGONAL_DOWN_RIGHT   0xB1
 
+#define MOVEMENT_ACTION_SHAKE_HORIZONTAL                0xB2
+#define MOVEMENT_ACTION_SHAKE_VERTICAL                  0xB3
+
 #define MOVEMENT_ACTION_STEP_END 0xFE
 #define MOVEMENT_ACTION_NONE     0xFF
 